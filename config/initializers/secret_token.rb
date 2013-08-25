221c79d84e3adddaaaf309f6e35869fff106ebfd
Tom Kiefhaber <tom.kiefhaber@hungrymachine.com>
2013-08-25 12:35:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rustychicken::Application.config.secret_key_base = '938dafba6bbad4ba6da10622ddecbafb9040a0e362bf523b4a7e353142c5e074aca43a828a371ff3d7698a2874d8f76e238f1d481ea6e5b82e5cabc96e1e0bbe'
